3e90874549e1416ca7c643e2fc2c4d74aee63d61
CyS-AYA <90491494+CyS-AYA@users.noreply.github.com>
2022-09-10 13:29:30 -0400

    Add files via upload

---
+++ isleapyear.c
@@ -0,0 +1,24 @@
+
+/*Ahmed Y. Ahmed
+FA22 CSGY-6233
+Assignment 2: Leap Year*/
+
+#include <stdio.h>
+
+void main(int argc, char** argv){
+    int year;
+    scanf("%d", &year);
+
+    if (year % 400 == 0) {
+        printf("%d was a leap year\n", year);
+        }
+    else if (year % 100 == 0) {
+        printf("%d was not a leap year\n", year);
+        }
+    else if (year % 4 == 0) {
+        printf("%d was a leap year\n", year);
+        }
+    else {
+        printf("%d was not a leap year\n", year);
+        }
+}
