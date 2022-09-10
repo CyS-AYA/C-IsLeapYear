5a2959728952be382911b46460f686472bd61327
CyS-AYA <90491494+CyS-AYA@users.noreply.github.com>
2022-09-09 23:21:22 -0400

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -4,20 +4,20 @@ Assignment 2: Leap Year*/
 
 #include <stdio.h>
 
-void isleapyear(int year){
+char isleapyear(int year){
     
       
     if (year % 400 == 0) {
-        printf("%d was a leap year\n", year);
+        return("%d was a leap year\n", year);
         } 
     else if (year % 100 == 0) {
-        printf("%d was not a leap year\n", year);
+        return("%d was not a leap year\n", year);
         } 
     else if (year % 4 == 0) {
-        printf("%d was a leap year\n", year);
+        return("%d was a leap year\n", year);
         } 
     else {
-        printf("%d was not a leap year\n", year);
+        return("%d was not a leap year\n", year);
         }
 } 
 
